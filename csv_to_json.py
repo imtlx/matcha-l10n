5b711b7a10f273e83f2faead0fdfa9dd68bdfb12
natroute <247222395+natroute@users.noreply.github.com>
2026-08-19 03:41:39 +0300

    add overrides

--- csv_to_json.py
+++ csv_to_json.py
@@ -8,13 +8,32 @@ import requests
 import sys
 from collections.abc import Iterable
 import re
+from typing import TypeAlias
 
 default_spreadsheet_id: str = "1koZhCQuZrVlRfv9vdfWF2huqhYliztvqEruG_0c_Mus"
-default_sheet_id: int = 1336669961
+default_main_sheet_id: int = 1336669961
+default_overrides_sheet_id: int = 1300505173
 
 escape_re = re.compile(r'\\u([0-9a-f]{4})', re.I)
 
-def csv_to_json(csv_it: Iterable[str], output_dir: Path):
+Overrides: TypeAlias = dict[str, dict[str, str]]
+
+def fetch_sheet(spreadsheet_id: int, sheet_id: int) -> list[str]:
+    url = f"https://docs.google.com/spreadsheets/d/{spreadsheet_id}/export"
+    params = {"gid": sheet_id, "format": "csv"}
+    print(f'Fetching: {url=!r}, {params=!r}', file=sys.stderr)
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.text.splitlines()
+
+def process_overrides_csv(csv_it: Iterable[str]) -> Overrides:
+    result: Overrides = {}
+    for lang, key, value in csv.reader(csv_it):
+        result.setdefault(lang, {})[key] = value
+    return result
+
+def csv_to_json(csv_it: Iterable[str], overrides: Overrides, output_dir: Path) -> None:
     output_dir.mkdir(exist_ok=True)
 
     rows = list(csv.reader(csv_it))
@@ -46,6 +65,9 @@ def csv_to_json(csv_it: Iterable[str], output_dir: Path):
             value = escape_re.sub(lambda m: chr(int(m[1], 16)), value)
             translations[key] = value
 
+        if locale in overrides:
+            translations |= overrides[locale]
+
         output_file = output_dir / f"{locale}.json"
         output = json.dumps(translations, ensure_ascii=False, indent=2)
         output = output.replace("\\\\n", "\\n")
@@ -55,25 +77,31 @@ def csv_to_json(csv_it: Iterable[str], output_dir: Path):
 
         print(f"Created: {output_file}", file=sys.stderr)
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         epilog=(
             "If 'file' is not provided, automatically fetches from Google "
             "Sheets with either the given spreadsheet and sheet IDs or the "
-            "script default."
+            "script defaults."
         )
     )
     parser.add_argument(
         "file", type=Path, nargs="?", default=None,
     )
     parser.add_argument(
-        "-S", "--spreadsheet-id", type=str, nargs="?", default=default_spreadsheet_id,
-        help=f"default: {default_spreadsheet_id}",
+        "overrides_file", type=Path, nargs="?", default=None,
     )
     parser.add_argument(
-        "-s", "--sheet-id", type=str, nargs="?", default=default_sheet_id,
-        help=f"default: {default_sheet_id}",
+        "-S", "--spreadsheet-id", type=str, nargs="?", default=default_spreadsheet_id,
+        help=f"default: {default_spreadsheet_id!r}",
+    )
+    parser.add_argument(
+        "-s", "--sheet-id", type=str, nargs="?", default=default_main_sheet_id,
+        help=f"default: {default_main_sheet_id!r}",
+    )
+    parser.add_argument(
+        "-O", "--overrides-sheet-id", type=str, nargs="?", default=default_overrides_sheet_id,
+        help=f"default: {default_overrides_sheet_id!r}",
     )
     parser.add_argument(
         "-o", "--output-dir", type=Path, nargs="?", default=Path("locales"),
@@ -81,19 +109,28 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    input_file: Path | None = args.file
-    spreadsheet_id: str = args.spreadsheet_id
-    sheet_id: str = args.sheet_id
+    main_file: Path | None = args.file
+    overrides_file: Path | None = args.overrides_file
+    spreadsheet_id: int = args.spreadsheet_id
+    main_sheet_id: int = args.sheet_id
+    overrides_sheet_id: int = args.overrides_sheet_id
     output_dir: Path = args.output_dir
 
-    if input_file is None:
-        url = f"https://docs.google.com/spreadsheets/d/{spreadsheet_id}/export"
-        params = {"gid": sheet_id, "format": "csv"}
-        print(f'Fetching: {url=!r}, {params=!r}', file=sys.stderr)
-
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        csv_to_json(response.text.splitlines(), output_dir)
+    overrides_csv: list[str]
+    if overrides_file is None:
+        overrides = process_overrides_csv(
+            fetch_sheet(spreadsheet_id, overrides_sheet_id)
+        )
     else:
-        with input_file.open("r", encoding="utf-8-sig") as f:
-            csv_to_json(f, output_dir)
+        with overrides_file.open("r", encoding="utf-8-sig") as f:
+            overrides = process_overrides_csv(f)
+
+    if main_file is None:
+        csv_to_json(
+            fetch_sheet(spreadsheet_id, main_sheet_id),
+            overrides,
+            output_dir
+        )
+    else:
+        with main_file.open("r", encoding="utf-8-sig") as f:
+            csv_to_json(f, overrides, output_dir)
